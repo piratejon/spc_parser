3be4c154bb2a3e621ca23f1e56b40668b0ff6a11
Kyle Creasey <lightning.jim@gmail.com>
2014-11-02 14:02:28 -0600

    Working better, using namespaces

--- spc_parser.py
+++ spc_parser.py
@@ -1,17 +1,32 @@
-import shapely, string, pykml
+#!/usr/bin/python2.7
+import shapely, string 
 from shapely.geometry import Polygon, Point
-#from pykml.factory import KML_ElementMaker as KML
-from pykml import parser
 from lxml import etree
 from os import path
-import xml.etree.ElementTree as ET
 
-day_1_cat_file = path.join("examples/day1otlk_cat.kml")
+XHTML_NAMESPACE = "http://earth.google.com/kml/2.2"
+XHTML = "{%s}" % XHTML_NAMESPACE
+NSMAP = {None : XHTML_NAMESPACE}
 
-with open(day_1_cat_file) as day_1_cat_open:
-  day_1_cat = parser.parse(day_1_cat_open)
+def sev_index(cat_type):
+  return {
+    'General Thunder' : 0,
+    'Marginal Risk' : 1,
+    'Slight Risk' : 2,
+    'Enhanced Risk' : 3,
+    'Moderate Risk' : 4,
+    'High Risk' : 5,
+  }.get(cat_type, -1)
+
+def polygon_parser(poly_elm):
+  return 0
+
+parser = etree.XMLParser(ns_clean=True)
+day_1_cat = etree.parse("examples/day2otlk_cat.kml", parser)
 root = day_1_cat.getroot()
-#print etree.iselement(root.Document.Folder.Placemark.Polygon.outerBoundaryIs.LinearRing.coordinates)
-#print etree.tostring(etree.ElementTree(root),pretty_print=True)
-child1 = root[1]
-print(child1.tag)
+risk_areas = root.findall(".//" + XHTML + "Placemark")
+for risk_area in risk_areas:
+  for child in risk_area:
+    if(child.tag == XHTML + "name"):
+      print child.text + " -> " + str(sev_index(child.text))
+
