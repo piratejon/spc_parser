bf744ecbafb52e156d1e4fa43372ef0991caa863
Kyle Creasey <kcreasey@localhost.localdomain>
2014-11-20 18:20:51 -0600

    Works within proper timezones

--- wx_alert.py
+++ wx_alert.py
@@ -1,10 +1,47 @@
 #!/usr/bin/python2.7
 import string
-from lxml import etree
 from os import path
 from optparse import OptionParser
+import feedparser
+import dateutil.parser as dateparser
+import datetime
 
-loc = 'OKZ025'
+def shorthand(alert_type):
+  return {
+    'Air Stagnation Advisory' : 'Air Stag Adv'
+  }.get(alert_type, "ERROR")
+
+#creating flags
+optParser = OptionParser()
+optParser.add_option("-s", "--short", "--conky", dest="short", action="store_true", help="Display shorthand output instead", default=False)
+optParser.add_option("-l", "--loc", dest="location", metavar="XXX###", help="Specifiy currently location Zone")
+(options, args) = optParser.parse_args()
+
+if(options.location is not None):
+  loc = options.location
+else:
+  loc = 'OKZ029'
 url = 'http://alerts.weather.gov/cap/wwaatmget.php?x=' + loc
-alert_tree = etree.parse(url).getroot();
+feed = feedparser.parse(url)
+if feed['entries'][0]['title'] == "There are no active watches, warnings or advisories":
+  print "No active alerts."
+else:
+  for alert in feed['entries']:
+	if options.short:
+		alert_type = shorthand(alert['cap_event'])
+	else:
+		alert_type = alert['cap_event']
+	alert_end = dateparser.parse(alert['cap_expires'])
+	alert_start = dateparser.parse(alert['cap_effective'])
+	if (alert_end.day > datetime.datetime.now(alert_end.tzinfo).day):
+		alert_string = alert_end.strftime(" until %a %I:%M %p")
+	else:
+		alert_string = alert_end.strftime(" until at %I:%M %p")
+	
+	if (alert_start > datetime.datetime.now(alert_start.tzinfo)):
+		if (alert_start.day > datetime.datetime.now(alert_start.tzinfo).day):
+			alert_string = alert_start.strftime("From %a %I:%M %p ") + alert_string
+		else:
+			alert_string = alert_start.strftime("From %I:%M %p ") + alert_string
 
+	print alert_type + alert_string
